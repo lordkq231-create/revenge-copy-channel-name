dfe358740039c0bcd5f1322422221e0e6edf2575
lordkq231-create <lordkq231@gmail.com>
2026-09-09 00:07:04 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-import { clipboard } from "@vendetta/clipboard";
+import { clipboard, logger, metro, patcher } from "@vendetta";
+
+const patches: (() => void)[] = [];
 
 export default {
     onLoad() {
-        console.log("[Copy Channel Name] Loaded");
+        logger.log("[Copy Channel Name] loaded");
+
+        const modules = metro.findAll(
+            (m: any) =>
+                m &&
+                typeof m === "object" &&
+                typeof m.default === "function" &&
+                String(m.default).includes("Channel")
+        );
+
+        for (const module of modules) {
+            try {
+                const unpatch = patcher.after(
+                    module,
+                    "default",
+                    (_args: any[], result: any) => result
+                );
+
+                patches.push(unpatch);
+            } catch {}
+        }
     },
 
     onUnload() {
-        console.log("[Copy Channel Name] Unloaded");
+        for (const unpatch of patches) {
+            try {
+                unpatch();
+            } catch {}
+        }
+
+        patches.length = 0;
     },
 };
