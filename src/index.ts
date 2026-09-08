98b97f05ab5fbfd496e72cc8f904135502cb6d5a
lordkq231-create <lordkq231@gmail.com>
2026-09-09 00:05:40 +0300

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { clipboard } from "@vendetta/clipboard";
+
+export default {
+    onLoad() {
+        console.log("[Copy Channel Name] Loaded");
+    },
+
+    onUnload() {
+        console.log("[Copy Channel Name] Unloaded");
+    },
+};
